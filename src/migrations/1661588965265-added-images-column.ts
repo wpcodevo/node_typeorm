0f6d3365f34c799a5d9a1f976a04922885650e00
Edem <ziddah.edem@gmail.com>
2022-08-27 08:48:53 +0000

    updated

--- src/migrations/1661588965265-added-images-column.ts
+++ src/migrations/1661588965265-added-images-column.ts
@@ -1,10 +1,9 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class addedImagesColumn1654508946547 implements MigrationInterface {
-    name = 'addedImagesColumn1654508946547'
+export class addedImagesColumn1661588965265 implements MigrationInterface {
+    name = 'addedImagesColumn1661588965265'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('user', 'admin')`);
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" "public"."users_role_enum" NOT NULL DEFAULT 'user', "photo" character varying NOT NULL DEFAULT 'default.png', "verified" boolean NOT NULL DEFAULT false, "verificationCode" text, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "email_index" ON "users" ("email") `);
         await queryRunner.query(`CREATE INDEX "verificationCode_index" ON "users" ("verificationCode") `);
@@ -18,7 +17,6 @@ export class addedImagesColumn1654508946547 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX "public"."verificationCode_index"`);
         await queryRunner.query(`DROP INDEX "public"."email_index"`);
         await queryRunner.query(`DROP TABLE "users"`);
-        await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
     }
 
 }
